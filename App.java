c62570b51724ebef2138d4428d167ad005c777d7
Sapto Aji <saosabc321@gmail.com>
2020-01-07 16:27:16 +0700

    Pengerjaan nomor 2

--- App.java
+++ App.java
@@ -1,16 +1,35 @@
 import java.util.Scanner;
 
+import javafx.scene.input.KeyCharacterCombination;
+
 public class App {
     public static void main(String[] args) {
         double beratKg;
-        double beratPon;
+        int beratPon;
 
         Scanner userInput = new Scanner(System.in);
+        System.out.println("Soal nomer 1 : ");
+        System.out.println();
         System.out.println("PROGRAM KONVERSI KG KE PON");
         System.out.print("Masukkan berat dalam KG : ");
         beratKg = userInput.nextDouble();
 
-        beratPon = beratKg * 2;
-        System.out.println("Berat dalam Pon dari " + beratKg + " Kg adalah " + beratPon + "Pon");
+        beratPon = (int) (beratKg * 2);
+        System.out.println("Berat dalam Pon dari " + beratKg + " Kg adalah " + beratPon + " Pon");
+        System.out.println();
+        System.out.println("Soal nomer 2 : ");
+        System.out.println();
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 != 0) {
+                System.out.println("Ganjil");
+            } else if (i % 2 == 0 && i % 3 == 0) {
+                System.out.println("INDONESIA");
+            } else if (i % 2 == 0) {
+                System.out.println(i);
+            }
+
+        }
+
     }
+
 }
